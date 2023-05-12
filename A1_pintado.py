3a0c3b4418df056506a450b78b52cec14489aa1b
LFelipe06 <a01735939@tec.mx>
2023-05-11 21:27:32 -0600

    Se añadio la funcion circle()

--- A1_pintado.py
+++ A1_pintado.py
@@ -1,29 +1,50 @@
+#Se importan las funciones de los módulos correspondientes
 from turtle import *
 from freegames import vector
+import math 
 
+#Se define la función para el dibujado de una línea recta
 def line(start, end):
     "Draw line from start to end."
+    #Se ejecuta up(), no se trazará nada mientras se mueve el cursor de al punto de inicio
     up()
+    #Se trazlada el cursor de dibujado a las coordenadas start.x y start.y
     goto(start.x, start.y)
+    #Se ejecuta down(), se trazará mientras se mueve el cursor de al punto de final
     down()
+    #Se trazlada el cursor de dibujado a las coordenadas end.x y end.y
     goto(end.x, end.y)
 
+#Se define la función para el dibujado de un cuadrado.
 def square(start, end):
     "Draw square from start to end."
+    #Se llama a up(), no se trazará mientras el cursor se posiciona en las coordenadas de inicio.
     up()
+    #Se trazlada el cursor de dibujado a las coordenadas start.x y start.y
     goto(start.x, start.y)
+    #Se llama a down(), se trazará mientras se mueve el cursor al punto deseado
     down()
+    #begin_fill() iniciara la función de rellenado de la figura dibujada 
     begin_fill()
-
+    #El ciclo for trazará una línea recta de largo igual a la diferencia entre start.x y end.x .
+    #Después del trazado, la dirección de dibujado rota 90 grados para posteriormente repetir el ciclo
     for count in range(4):
         forward(end.x - start.x)
         left(90)
-
+    #Se llama a la función end_fill(), esta rellena la figura trazada
     end_fill()
 
+#Se define la función para el dibujado de un circulo relleno
 def circle(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    #Se desplaza el cursor al punto medio entre las coordenadas del tap inicial y final
+    goto((start.x+end.x)/2,(start.y+end.y)/2)
+    down()
+    #Se calcula el diametro del circulo a trazar
+    diametro=math.sqrt(math.pow(end.x-start.x,2)+math.pow(end.y-start.y,2))
+    #Se traza el circulo con diametro delimitado por tap inicial y final
+    dot(diametro, "black")
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -32,7 +53,7 @@ def rectangle(start, end):
 def triangle(start, end):
     "Draw triangle from start to end."
     pass  # TODO
-
+#Para cada clic en el area de dibujado, se almacenaran las coordenadas x,y del cursor
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
